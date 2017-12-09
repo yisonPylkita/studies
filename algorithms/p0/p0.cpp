17acda5719ed39446285ae58059aa407665730da
yisonPylkita <yisonPylkita@gmail.com>
2017-12-09 15:53:25 +0100

    Replaced tabs with 4 spaces

--- algorithms/p0/p0.cpp
+++ algorithms/p0/p0.cpp
@@ -32,30 +32,30 @@ unsigned arithmetic_mean(const T &arr)
 template <typename T>
 void print_table(const T &data, const std::string &name, unsigned line_size)
 {
-	const unsigned data_size = data.size();
-		if (data_size == 0) {
-			std::cout << "Table " << name << " is empty\n";
-			return;
-		}
-		std::cout << "Table " << name << ", size "<< data_size << "\n";
+    const unsigned data_size = data.size();
+        if (data_size == 0) {
+            std::cout << "Table " << name << " is empty\n";
+            return;
+        }
+        std::cout << "Table " << name << ", size "<< data_size << "\n";
 
-		
-		unsigned curr_line_size = 0;
-		for (unsigned i = 0; i < data_size; i++) {
-			std::cout << data[i] << ", ";
-			curr_line_size++;
-			if (curr_line_size > line_size) {
-				curr_line_size = 0;
-				std::cout << "\n";
-			}
-		}
+        
+        unsigned curr_line_size = 0;
+        for (unsigned i = 0; i < data_size; i++) {
+            std::cout << data[i] << ", ";
+            curr_line_size++;
+            if (curr_line_size > line_size) {
+                curr_line_size = 0;
+                std::cout << "\n";
+            }
+        }
 }
 
 template <typename T>
 void selection_sort(T &vec)
 {
-	const auto vec_size = vec.size();
-	unsigned min_elem = 0;
+    const auto vec_size = vec.size();
+    unsigned min_elem = 0;
     for (int i = 0; i < vec_size; i++) {
         min_elem = i;
         for (int j = i + 1; j < vec_size; j++)
@@ -70,7 +70,7 @@ int main()
     constexpr std::size_t n = 1000000;
     constexpr unsigned d = 100;
     constexpr unsigned g = 100000;
-	constexpr unsigned k = 8;
+    constexpr unsigned k = 8;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(d, g);
@@ -79,29 +79,29 @@ int main()
     for (auto &x : A)
         x = dis(gen);
      unsigned average = arithmetic_mean(A);
-	 print_table(A, "A", k);
-	 
-	 // 1.
-	 std::vector<unsigned> B;
-	 std::vector<unsigned> C;
-	 for (auto x : A) {
-		 if (x > average)
-			 B.push_back(x);
-		 else
-			 C.push_back(x);
-	 }
+     print_table(A, "A", k);
+     
+     // 1.
+     std::vector<unsigned> B;
+     std::vector<unsigned> C;
+     for (auto x : A) {
+         if (x > average)
+             B.push_back(x);
+         else
+             C.push_back(x);
+     }
 
-	 // 2.
-	 print_table(B, "B", k);
-	 print_table(C, "C", k);
+     // 2.
+     print_table(B, "B", k);
+     print_table(C, "C", k);
 
-	 // 3.
-	 selection_sort(A);
-	 selection_sort(B);
-	 selection_sort(C);
+     // 3.
+     selection_sort(A);
+     selection_sort(B);
+     selection_sort(C);
 
-	 //4.
-	 print_table(A, "B", k);
-	 print_table(B, "B", k);
-	 print_table(C, "C", k);
+     //4.
+     print_table(A, "B", k);
+     print_table(B, "B", k);
+     print_table(C, "C", k);
 }
